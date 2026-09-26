69355540c33984982d29c1790e8f6e3e2d294796
TorasJnr <rasheedidris416@gmail.com>
2026-09-26 17:23:47 +0100

    mongoose connect

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 
 import express from "express";
-
-import userRoutes from "./routes/userRoutes.js"
+import userRoutes from "./routes/userRoutes.js";
+import mongoose from "mongoose";
+import dotenv from "dotenv"
+dotenv.config()
 
 const app = express();
 
 const PORT = 3000;
+
+mongoose.connect(process.env.MONGODB_URI).then(() => console.log("Database connected successfully"))
+.catch((err) => console.error(err))
 
 app.use(express.json())
 app.use("/api", userRoutes)
